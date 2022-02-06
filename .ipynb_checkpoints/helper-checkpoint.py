de6ab9331d599fa7b3132bc83de379438182e067
aanmar <maryanto.aan@gmail.com>
2022-02-06 20:13:47 +0700

    First Commit

---
+++ .ipynb_checkpoints/helper-checkpoint.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+class Helper():
+    def __init__():
+        super().__init__()
+        
+    def df_from_csv(url):
+        df = pd.read_csv(url)
+        return df
+    
+    def df_from_excel(url):
+        df = pd.read_excel(url)
+        return df
---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,9 @@
+for i in range(100):
+    if(i%3==0 & i%5==0):
+        print("tet tot")
+    elif(i%3==0):
+        print("tet")
+    elif(i%5==0):
+        print("tot")
+    else:
+        print(i)
